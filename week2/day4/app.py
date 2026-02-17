2718a3ff25bf1488d65eaa0853f9e708f7cf5d79
Anup Kumar <anupkumar23111992@gmail.com>
2026-02-18 00:11:06 +0530

    Week 2 Day 4: Flask + MYSQL Integration

---
+++ week2/day4/app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request
+import mysql.connector
+
+app = Flask(__name__)
+
+def get_connection():
+    try:
+        conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Password",
+            database="student_detail"
+        )
+        return conn
+    except mysql.connector.Error as err:
+        print("Database Connection Error: ", err)
+        return None
+    
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        name = request.form.get("name")
+        age = request.form.get("age")
+        course = request.form.get("course")
+
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        query = "INSERT INTO students (name, age, course) VALUES(%s, %s, %s)"
+        values = (name, age, course)
+        cursor.execute(query, values)
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+    return render_template("home.html")
+
+@app.route("/students")
+def students():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM students")
+    data = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return render_template("students.html", students=data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
